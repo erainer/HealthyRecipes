b2220a1d211ed7a565142e81bae3778e6efb9c5f
erainer <erainer@students.mchenry.edu>
2018-01-26 20:16:00 -0600

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/healthyrecipes/MainActivity.java
@@ -0,0 +1,24 @@
+package net.androidbootcamp.healthyrecipes;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button btnRecipe = (Button) findViewById(R.id.btnRecipe);
+
+        btnRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Recipe.class));
+            }
+        });
+    }
+}
